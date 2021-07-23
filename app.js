f5cd498d4066d59e379120fd0096cb3e12f1d417
jesica <jesicaguerrerof@gmail.com>
2021-07-23 18:30:28 -0300

    crea estructura html

---
+++ app.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app=express();
+const port=3030;
+const path=require('path');
+app.use(express.static('public'));
+app.listen(port,()=>console.log(`servidor corriendo en http://localhost:${port}`));
+app.get("/", (req,res)=>res.sendFile(path.join(__dirname, "views","home.html")));
